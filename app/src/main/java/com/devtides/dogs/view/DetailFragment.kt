7ce3fc8f30b1ccf0186b2b4f98aa5634227e3d30
leoallvez <leoallvez@hotmail.com>
2020-01-12 19:24:52 -0300

    Screen layout and navigation setup.

--- app/src/main/java/com/devtides/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/devtides/dogs/view/DetailFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.devtides.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +22,14 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
