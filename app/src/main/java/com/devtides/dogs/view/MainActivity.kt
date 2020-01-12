2df0f28303cdf3ee2de3b9c832bb52f6ff3508f8
leoallvez <leoallvez@hotmail.com>
2020-01-12 00:54:24 -0300

    first commit

---
+++ app/src/main/java/com/devtides/dogs/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.devtides.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devtides.dogs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
