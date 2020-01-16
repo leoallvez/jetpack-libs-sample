7fc3fb15cb915b8947e324afcedd28bfad84f3eb
leoallvez <leoallvez@hotmail.com>
2020-01-15 23:01:04 -0300

    model

---
+++ app/src/main/java/com/devtides/dogs/model/Model.kt
@@ -0,0 +1,11 @@
+package com.devtides.dogs.model
+
+data class DogBreed (
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val breedFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
