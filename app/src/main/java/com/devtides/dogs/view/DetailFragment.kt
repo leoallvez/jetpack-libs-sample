93630f8894b530a73c3cc32e58da0a4dcaceafb3
leoallvez <leoallvez@hotmail.com>
2020-01-21 22:59:36 -0300

    palette

--- app/src/main/java/com/devtides/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/devtides/dogs/view/DetailFragment.kt
@@ -1,6 +1,8 @@
 package com.devtides.dogs.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,8 +11,13 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.devtides.dogs.R
 import com.devtides.dogs.databinding.FragmentDetailBinding
+import com.devtides.dogs.model.DogPalette
 import com.devtides.dogs.viewmodel.DetailViewModel
 
 /**
@@ -44,11 +51,28 @@ class DetailFragment : Fragment() {
     }
 
     private fun observeViewModel() {
-        //TODO: ver se realmente é necessário esse observe;
         viewModel.dogLiveData.observe(this, Observer { dog ->
-            dog?.let {
+            dog?.let { it ->
                 dataBinding.dog = dog
+                it.imageUrl?.let { url -> setupBackgroundColor(url) }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate {palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            dataBinding.palette = DogPalette(intColor)
+                        }
+                }
+            })
     }
 }
